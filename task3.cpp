c19f72e64b61c82b91c68c8149908723de28e8df
Kirill <1altumsilentium1@gmail.com>
2022-12-05 23:03:12 +0300

    Fixed

--- task3.cpp
+++ task3.cpp
@@ -10,19 +10,25 @@ struct Address
     unsigned int flat_num = 0;
     unsigned int index = 0;
 };
-void OutputInfo(Address* city,Address* street,Address* house_num,Address* flat_num,Address* index)
+void OutputInfo(Address )
 {
-    std::cout << "Город: ";
-    std::cin >> city->city;  
-    std::cout << "Улица: ";
-    std::cin >> street->street;
-    std::cout << "Номер дома: ";
-    std::cin >> house_num->house_num;   
-    std::cout << "Номер квартиры: ";
-    std::cin >> flat_num->flat_num;   
-    std::cout << "Индекс: ";
-    std::cin >> index->index;
+    Address city;
+    std::cin >> city.city;   
+    Address street;
+    std::cin >> street.street;   
+    Address house_num;
+    std::cin >> house_num.house_num;  
+    Address flat_num;
+    std::cin >> flat_num.flat_num;
+    Address index;
+    std::cin >> index.index;
     std::cout << std::endl;
+    std::cout << "Информация: " << std::endl;
+    std::cout << "Город: " << city.city << std::endl;
+    std::cout << "Улица: " << street.street << std::endl;
+    std::cout << "Номер дома: " << house_num.house_num << std::endl;
+    std::cout << "Номер кваритры: " << flat_num.flat_num << std::endl;
+    std::cout << "Индекс: " << index.index << std::endl;
 }
 
 int main(int argc,char** argv)
@@ -30,12 +36,8 @@ int main(int argc,char** argv)
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
-    Address city;
-    Address street;
-    Address house_num;
-    Address flat_num;
-    Address index;
-    OutputInfo(&city, &street, &house_num, &flat_num, &index);
-    OutputInfo(&city, &street, &house_num, &flat_num, &index);
+    Address A;
+    OutputInfo(A);
+    OutputInfo(A);
     return 0;
 }
